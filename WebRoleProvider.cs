2d6586315db75a423b9967857a78c0bf564f02d0
ahsagar007 <33182440+ahsagar007@users.noreply.github.com>
2021-01-24 11:14:50 +0600

    Add files via upload

---
+++ WebRoleProvider.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace AidHandSociety
+{
+    public class WebRoleProvider : RoleProvider
+    {
+        public override string Name => base.Name;
+
+        public override string Description => base.Description;
+
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            using (var context = new AidHandSocietyEntities())
+            {
+                var result = (from user in context.Users
+                    join role in context.UserRoles on user.Id equals role.UserId
+                    where user.UserName == username
+                    select role.Role).ToArray();
+                return result;
+            }
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
